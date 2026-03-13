b1dee96e1a9bbf79faf29b6ea692f3feeadcce9f
khoi <6994441+khoi@users.noreply.github.com>
2026-03-13 21:02:28 +0000

    Remove development-flag-driven update gating logic

--- supaterm/App/TerminalCommands.swift
+++ supaterm/App/TerminalCommands.swift
@@ -6,6 +6,24 @@ struct TerminalCommands: Commands {
   let store: StoreOf<UpdateFeature>
 
   var body: some Commands {
+    let isDevelopmentBuild: Bool = {
+      #if DEBUG
+        return true
+      #else
+        guard let value = Bundle.main.object(forInfoDictionaryKey: "SupatermDevelopmentBuild") else {
+          return false
+        }
+        switch value {
+        case let boolValue as Bool:
+          return boolValue
+        case let stringValue as String:
+          ["1", "true", "yes"].contains(stringValue.trimmingCharacters(in: .whitespacesAndNewlines).lowercased())
+        default:
+          return false
+        }
+      #endif
+    }()
+
     CommandGroup(replacing: .sidebar) {
       Button("Toggle Sidebar") {
         NotificationCenter.default.post(name: .toggleSidebar, object: nil)
@@ -14,7 +32,7 @@ struct TerminalCommands: Commands {
     }
 
     CommandGroup(after: .appInfo) {
-      if AppBuild.isDevelopment {
+      if isDevelopmentBuild {
         Button("This is a development build") {}
           .disabled(true)
       }
--- supatermTests/AppBuildTests.swift
+++ supatermTests/AppBuildTests.swift
@@ -4,24 +4,7 @@ import Testing
 
 struct AppBuildTests {
   @Test
-  func developmentFlagParsesTrueValues() {
-    #expect(AppBuild.isDevelopmentFlag(true))
-    #expect(AppBuild.isDevelopmentFlag("YES"))
-    #expect(AppBuild.isDevelopmentFlag("true"))
-    #expect(AppBuild.isDevelopmentFlag(" 1 "))
-  }
-
-  @Test
-  func developmentFlagParsesFalseValues() {
-    #expect(!AppBuild.isDevelopmentFlag(nil))
-    #expect(!AppBuild.isDevelopmentFlag(false))
-    #expect(!AppBuild.isDevelopmentFlag("NO"))
-    #expect(!AppBuild.isDevelopmentFlag("0"))
-  }
-
-  @Test
-  func backgroundUpdateCheckOnLaunchIsEnabledForAllBuilds() {
-    #expect(AppBuild.allowsBackgroundUpdateCheckOnLaunch(isDevelopment: true))
-    #expect(AppBuild.allowsBackgroundUpdateCheckOnLaunch(isDevelopment: false))
+  func backgroundUpdateCheckOnLaunchIsAlwaysEnabled() {
+    #expect(AppBuild.allowsBackgroundUpdateCheckOnLaunch)
   }
 }
