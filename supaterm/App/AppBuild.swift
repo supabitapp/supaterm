a2e015bdf9d69af0dadefd4cfbcdaefeddcaaa8a
khoi <6994441+khoi@users.noreply.github.com>
2026-03-13 21:02:28 +0000

    Remove development-flag-driven update gating logic

--- supaterm/App/AppBuild.swift
+++ supaterm/App/AppBuild.swift
@@ -1,30 +1,7 @@
 import Foundation
 
 enum AppBuild {
-  static var isDevelopment: Bool {
-    #if DEBUG
-      true
-    #else
-      isDevelopmentFlag(Bundle.main.object(forInfoDictionaryKey: "SupatermDevelopmentBuild"))
-    #endif
-  }
-
   static var allowsBackgroundUpdateCheckOnLaunch: Bool {
-    allowsBackgroundUpdateCheckOnLaunch(isDevelopment: isDevelopment)
-  }
-
-  static func isDevelopmentFlag(_ rawValue: Any?) -> Bool {
-    switch rawValue {
-    case let value as Bool:
-      value
-    case let value as String:
-      ["1", "true", "yes"].contains(value.trimmingCharacters(in: .whitespacesAndNewlines).lowercased())
-    default:
-      false
-    }
-  }
-
-  static func allowsBackgroundUpdateCheckOnLaunch(isDevelopment: Bool) -> Bool {
     true
   }
 }
