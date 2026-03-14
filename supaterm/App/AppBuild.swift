605ed828b1e6bf5e6d96ba03eb5137e3aa35a1cf
khoi <6994441+khoi@users.noreply.github.com>
2026-03-14 19:50:36 +0000

    Show development builds in update pill

--- supaterm/App/AppBuild.swift
+++ supaterm/App/AppBuild.swift
@@ -1,7 +1,31 @@
 import Foundation
 
 enum AppBuild {
+  static let developmentBuildMessage = "This is a development build"
+  static let developmentPillText = "Development Build"
+
   static var allowsBackgroundUpdateCheckOnLaunch: Bool {
     true
+  }
+
+  static var isDevelopmentBuild: Bool {
+    #if DEBUG
+      true
+    #else
+      isDevelopmentFlag(Bundle.main.object(forInfoDictionaryKey: "SupatermDevelopmentBuild"))
+    #endif
+  }
+
+  static func isDevelopmentFlag(_ value: Any?) -> Bool {
+    switch value {
+    case let boolValue as Bool:
+      return boolValue
+    case let stringValue as String:
+      return ["1", "true", "yes"].contains(stringValue.trimmingCharacters(in: .whitespacesAndNewlines).lowercased())
+    case let numberValue as NSNumber:
+      return numberValue.boolValue
+    default:
+      return false
+    }
   }
 }
--- supaterm/App/TerminalCommands.swift
+++ supaterm/App/TerminalCommands.swift
@@ -6,24 +6,6 @@ struct TerminalCommands: Commands {
   let store: StoreOf<AppFeature>
 
   var body: some Commands {
-    let isDevelopmentBuild: Bool = {
-      #if DEBUG
-        return true
-      #else
-        guard let value = Bundle.main.object(forInfoDictionaryKey: "SupatermDevelopmentBuild") else {
-          return false
-        }
-        switch value {
-        case let boolValue as Bool:
-          return boolValue
-        case let stringValue as String:
-          return ["1", "true", "yes"].contains(stringValue.trimmingCharacters(in: .whitespacesAndNewlines).lowercased())
-        default:
-          return false
-        }
-      #endif
-    }()
-
     CommandGroup(replacing: .sidebar) {
       Button("Toggle Sidebar") {
         NotificationCenter.default.post(name: .toggleSidebar, object: nil)
@@ -70,11 +52,6 @@ struct TerminalCommands: Commands {
     }
 
     CommandGroup(after: .appInfo) {
-      if isDevelopmentBuild {
-        Button("This is a development build") {}
-          .disabled(true)
-      }
-
       Button("Check for Updates...") {
         store.send(.update(.checkForUpdatesButtonTapped))
       }
