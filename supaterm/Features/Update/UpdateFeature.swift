c3ec2516a6e97117646b18fd5db77075e20a28c0
khoi <6994441+khoi@users.noreply.github.com>
2026-03-14 23:18:54 +0000

    Revert "Stub update animation"
    This reverts commit 6a23cb12b22e6e505673ce22435d0cc64b74771c.

--- supaterm/Features/Update/UpdateFeature.swift
+++ supaterm/Features/Update/UpdateFeature.swift
@@ -5,26 +5,6 @@ private enum UpdateFeatureCancelID {
   static let observation = "UpdateFeature.observation"
 }
 
-private enum UpdateFeatureDebugDemo {
-  nonisolated static let checkingDuration: Duration = .seconds(1)
-  nonisolated static let downloadExpectedLength: UInt64 = 146_800_640
-  nonisolated static let downloadFractions: [Double] = [0.18, 0.42, 0.67, 0.86, 1]
-  nonisolated static let downloadStepDuration: Duration = .milliseconds(350)
-  nonisolated static let extractingFractions: [Double] = [0.2, 0.5, 0.8, 1]
-  nonisolated static let extractingStepDuration: Duration = .milliseconds(300)
-  nonisolated static let updateAvailableDuration: Duration = .seconds(1)
-  nonisolated static let updateInfo = UpdateInfo(
-    contentLength: downloadExpectedLength,
-    publishedAt: nil,
-    releaseNotesURL: nil,
-    version: "0.4.0"
-  )
-
-  nonisolated static func snapshot(phase: UpdatePhase) -> UpdateClient.Snapshot {
-    .init(canCheckForUpdates: true, phase: phase)
-  }
-}
-
 @Reducer
 struct UpdateFeature {
   @ObservableState
@@ -48,6 +28,7 @@ struct UpdateFeature {
   enum Action {
     case allowAutomaticUpdatesButtonTapped
     case checkForUpdatesButtonTapped
+    case debugStubCheckFinished
     case developmentBuildHoverChanged(Bool)
     case dismissButtonTapped
     case installAndRelaunchButtonTapped
@@ -84,58 +65,19 @@ struct UpdateFeature {
         state.phase = .checking
         if appBuildClient.usesStubUpdateChecks() {
           return .run { [clock] send in
-            try? await clock.sleep(for: UpdateFeatureDebugDemo.checkingDuration)
-            await send(
-              .updateClientSnapshotReceived(
-                UpdateFeatureDebugDemo.snapshot(
-                  phase: .updateAvailable(UpdateFeatureDebugDemo.updateInfo)
-                )
-              )
-            )
-
-            try? await clock.sleep(for: UpdateFeatureDebugDemo.updateAvailableDuration)
-            for fraction in UpdateFeatureDebugDemo.downloadFractions {
-              await send(
-                .updateClientSnapshotReceived(
-                  UpdateFeatureDebugDemo.snapshot(
-                    phase: .downloading(
-                      .init(
-                        expectedLength: UpdateFeatureDebugDemo.downloadExpectedLength,
-                        receivedLength: UInt64(
-                          Double(UpdateFeatureDebugDemo.downloadExpectedLength) * fraction
-                        )
-                      )
-                    )
-                  )
-                )
-              )
-              try? await clock.sleep(for: UpdateFeatureDebugDemo.downloadStepDuration)
-            }
-
-            for fraction in UpdateFeatureDebugDemo.extractingFractions {
-              await send(
-                .updateClientSnapshotReceived(
-                  UpdateFeatureDebugDemo.snapshot(
-                    phase: .extracting(fraction)
-                  )
-                )
-              )
-              try? await clock.sleep(for: UpdateFeatureDebugDemo.extractingStepDuration)
-            }
-
-            await send(
-              .updateClientSnapshotReceived(
-                UpdateFeatureDebugDemo.snapshot(
-                  phase: .installing(.init(canInstallNow: true))
-                )
-              )
-            )
+            try? await clock.sleep(for: .seconds(1))
+            await send(.debugStubCheckFinished)
           }
           .cancellable(id: UpdateFeatureCancelID.debugStubCheck, cancelInFlight: true)
         }
         return .run { [updateClient] _ in
           await updateClient.checkForUpdates()
         }
+
+      case .debugStubCheckFinished:
+        guard case .checking = state.phase else { return .none }
+        state.phase = .idle
+        return .none
 
       case .developmentBuildHoverChanged(let isHovering):
         guard state.isDevelopmentBuild else { return .none }
@@ -148,20 +90,13 @@ struct UpdateFeature {
         state.isDevelopmentIndicatorHovering = false
         state.isPopoverPresented = false
         state.phase = .idle
-        return .merge(
-          .cancel(id: UpdateFeatureCancelID.debugStubCheck),
-          .run { [updateClient] _ in
-            await updateClient.sendIntent(.dismiss)
-          }
-        )
+        return .run { [updateClient] _ in
+          await updateClient.sendIntent(.dismiss)
+        }
 
       case .installAndRelaunchButtonTapped:
         state.isDevelopmentIndicatorHovering = false
         state.isPopoverPresented = false
-        if appBuildClient.usesStubUpdateChecks() {
-          state.phase = .downloading(.init(expectedLength: nil, receivedLength: 0))
-          return self.stubInstallSequenceEffect()
-        }
         return .run { [updateClient] _ in
           await updateClient.sendIntent(.install)
         }
@@ -170,12 +105,9 @@ struct UpdateFeature {
         state.isDevelopmentIndicatorHovering = false
         state.isPopoverPresented = false
         state.phase = .idle
-        return .merge(
-          .cancel(id: UpdateFeatureCancelID.debugStubCheck),
-          .run { [updateClient] _ in
-            await updateClient.sendIntent(.later)
-          }
-        )
+        return .run { [updateClient] _ in
+          await updateClient.sendIntent(.later)
+        }
 
       case .pillButtonTapped:
         guard !state.phase.isIdle else { return .none }
@@ -197,10 +129,6 @@ struct UpdateFeature {
       case .restartNowButtonTapped:
         state.isDevelopmentIndicatorHovering = false
         state.isPopoverPresented = false
-        if appBuildClient.usesStubUpdateChecks() {
-          state.phase = .idle
-          return .cancel(id: UpdateFeatureCancelID.debugStubCheck)
-        }
         return .run { [updateClient] _ in
           await updateClient.sendIntent(.restartNow)
         }
@@ -209,23 +137,17 @@ struct UpdateFeature {
         state.isDevelopmentIndicatorHovering = false
         state.isPopoverPresented = false
         state.phase = .idle
-        return .merge(
-          .cancel(id: UpdateFeatureCancelID.debugStubCheck),
-          .run { [updateClient] _ in
-            await updateClient.sendIntent(.retry)
-          }
-        )
+        return .run { [updateClient] _ in
+          await updateClient.sendIntent(.retry)
+        }
 
       case .skipButtonTapped:
         state.isDevelopmentIndicatorHovering = false
         state.isPopoverPresented = false
         state.phase = .idle
-        return .merge(
-          .cancel(id: UpdateFeatureCancelID.debugStubCheck),
-          .run { [updateClient] _ in
-            await updateClient.sendIntent(.skip)
-          }
-        )
+        return .run { [updateClient] _ in
+          await updateClient.sendIntent(.skip)
+        }
 
       case .task:
         state.isDevelopmentBuild = appBuildClient.isDevelopmentBuild()
@@ -263,47 +185,5 @@ struct UpdateFeature {
         return .none
       }
     }
-  }
-
-  private func stubInstallSequenceEffect() -> Effect<Action> {
-    .run { [clock] send in
-      for fraction in UpdateFeatureDebugDemo.downloadFractions {
-        await send(
-          .updateClientSnapshotReceived(
-            UpdateFeatureDebugDemo.snapshot(
-              phase: .downloading(
-                .init(
-                  expectedLength: UpdateFeatureDebugDemo.downloadExpectedLength,
-                  receivedLength: UInt64(
-                    Double(UpdateFeatureDebugDemo.downloadExpectedLength) * fraction
-                  )
-                )
-              )
-            )
-          )
-        )
-        try? await clock.sleep(for: UpdateFeatureDebugDemo.downloadStepDuration)
-      }
-
-      for fraction in UpdateFeatureDebugDemo.extractingFractions {
-        await send(
-          .updateClientSnapshotReceived(
-            UpdateFeatureDebugDemo.snapshot(
-              phase: .extracting(fraction)
-            )
-          )
-        )
-        try? await clock.sleep(for: UpdateFeatureDebugDemo.extractingStepDuration)
-      }
-
-      await send(
-        .updateClientSnapshotReceived(
-          UpdateFeatureDebugDemo.snapshot(
-            phase: .installing(.init(canInstallNow: true))
-          )
-        )
-      )
-    }
-    .cancellable(id: UpdateFeatureCancelID.debugStubCheck, cancelInFlight: true)
   }
 }
