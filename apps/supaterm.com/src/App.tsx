0c904814eb8a4a391d4d613932fc18056ba5a206
khoi <6994441+khoi@users.noreply.github.com>
2026-03-27 10:18:15 +0100

    Show only single download button on web homepage

--- apps/supaterm.com/src/App.tsx
+++ apps/supaterm.com/src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 function App() {
   return (
     <main id="center" className="single-download">
-      <h1>Download SupaTerm</h1>
       <a className="counter" href="https://supaterm.com/download/tip/supaterm.dmg">
         Download
       </a>
