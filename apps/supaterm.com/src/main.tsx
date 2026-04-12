4f0f9c010c7d30675d3aa8b107c8d9ea645db4b6
khoi <6994441+khoi@users.noreply.github.com>
2026-04-12 17:20:42 +0100

    Use proxied PostHog config on supaterm.com

--- apps/supaterm.com/src/main.tsx
+++ apps/supaterm.com/src/main.tsx
@@ -5,7 +5,10 @@ import "./index.css";
 import App from "./App.tsx";
 
 posthog.init("phc_AwJsG6OgXpxwREkX5OW41cZ1tjjoLplTif5KbocleFx", {
-  api_host: "https://us.i.posthog.com",
+  api_host: "https://p.supaterm.com",
+  ui_host: "https://us.posthog.com",
+  defaults: "2026-01-30",
+  person_profiles: "identified_only",
 });
 
 createRoot(document.getElementById("root")!).render(
